54e3e392e378b811f47ca1d78023db956fa1c617
Mohd Musaiyab <mohdmusaiyab123@gmail.com>
2024-03-25 20:53:30 +0530

    Modifications

--- index.ts
+++ index.ts
@@ -1,18 +1,16 @@
 import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
-import userRoutes from "./routes/userRoutes";
-import categoryRoutes from "./routes/categoryRoutes";
+import cors from 'cors'
+
 import { dbConnect } from "./config/dbConnect";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors());
 // =========================User Routes=======================
-app.use("/api/v1/user", userRoutes);
-// ===================Category Routes======================= 
-app.use('/api/v1/category', categoryRoutes);
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
