26648ddd2113fb15f104d48e52eab770b33c64d4
Mohd Musaiyab <mohdmusaiyab123@gmail.com>
2024-04-08 18:20:43 +0530

    Middlewares and User Controllers Added

--- middlewares/isSign.ts
+++ middlewares/isSign.ts
@@ -1,29 +1,35 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
-// Middleware function to check if user is authenticated
-function authenticateUser(req: Request, res: Response, next: NextFunction) {
-  // Extract the token from the request headers
-  const token = req.headers.authorization?.split(" ")[1];
+export const authenticateUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  // Get the token from the Authorization header
+  const token = req.headers.authorization;
+  // const token = authHeader && authHeader.split(' ')[1];
+  // console.log(token)
 
-  // If token is not provided, proceed without attaching user information
   if (!token) {
-    return next(); // Proceed to the next middleware or route handler
+    // If there's no token, the user is not authenticated
+    return res.status(401).json({ message: "Not authenticated" });
   }
 
   try {
     // Verify the token
     const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
-
-    // Attach the decoded user information to the request object
-    (req as any).user = decoded; // Using type assertion to avoid TypeScript errors
-
-    // Proceed to the next middleware or route handler
+    // console.log("Decoded values",decoded);
+    // Add the user data to the request object
+    const{_id}=decoded as {_id:string}
+    // console.log("User id",_id)
+    req.userId=_id;
+    // console.log("User id",req.userId)
     next();
   } catch (error) {
-    // If token verification fails, return unauthorized error
-    return res.status(401).json({ error: "Unauthorized" });
+    // If token verification fails, return an error
+    return res.status(401).json({ message: "Invalid token" });
   }
-}
+};
 
-export { authenticateUser };
+export default authenticateUser;
